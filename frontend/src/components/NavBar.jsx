34e094bb56a492f18bb2c5506d9be89fb88d47ba
betaniagonzalez31@gmail.com <betaniagonzalez31@gmail.com>
2022-10-21 19:37:41 -0300

    fixed nav

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,11 +7,11 @@ import { CartWidget } from './cart/CartWidget'
 export const NavBar = () => {
     const { productsCart } = useContext(firebaseContext)
     return (
-        <nav className="navbar navbar-expand-lg mb-4" >
+        <nav className=" navbar navbar-expand-lg mb-4" >
             <Link to='/' className='fs-3 ubuntu navbar-brand'>fire<span className='text-primary'>base</span></Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" />
+            {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation" /> */}
             <div className=" collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-                <div className="navbar-nav  fs-5">
+                <div className=" navbar-nav fs-5">
                     <NavLink to='/category/Romance' className="nav-link" >Romance</NavLink>
                     <NavLink to='/category/Drama' className="nav-link" >Drama</NavLink>
                     <NavLink to='/category/Action' className="nav-link" >Action</NavLink>
